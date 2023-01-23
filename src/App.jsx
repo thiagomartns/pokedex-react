3b0d618d5cb001b56213e2e390e96ddb5252c20d
thiago <tmg3.mn@gmail.com>
2023-01-23 13:35:18 -0300

    Interação com a API adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.scss'
 import PokeCard from './Components/pokeCard/PokeCard';
+import { useFetch } from './hooks/useFetch'
 
 function App() {
+
+  const { pokemons, setPokemons } = useFetch()
+
   return (
     <div className="App">
       <h1>Pokedex</h1>
+      <p>First Generation</p>
       <div className="poke-container" id="pokecontainer">
-        <PokeCard />
+        {pokemons.map((item, index) => (
+          <PokeCard item={item.data} index={index} />
+        ))}
       </div>
     </div>
   );
--- src/Components/pokeCard/PokeCard.jsx
+++ src/Components/pokeCard/PokeCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './pokeCard.scss'
 
-const PokeCard = () => {
+const PokeCard = ({ index, item }) => {
+
   return (
     <>
-      <div className="pokemon pokemon-type">
+      <div className="pokemon pokemon-type" key={index}>
         <div className="img-container">
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${item.id}.png`} alt="" />
         </div>
         <div className="info">
-          <span className="number">#001</span>
-          <h3 className="name">Bulbassaur</h3>
-          <h5 className="type">Grass</h5>
+          <span className="number">#{item.id}</span>
+          <h3 className="name">{item.species.name}</h3>
+          <h5 className="type">{item.types[0].type.name}</h5>
         </div>
       </div>
     </>
