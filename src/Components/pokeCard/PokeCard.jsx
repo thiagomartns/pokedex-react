1ee5ca993bd6f8c73ad5ce230d8b9ab439bae4dc
thiago <tmg3.mn@gmail.com>
2023-01-23 10:43:55 -0300

    HTML e estilos adicionados em 'PokeCard'

--- src/Components/pokeCard/PokeCard.jsx
+++ src/Components/pokeCard/PokeCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './pokeCard.scss'
 
 const PokeCard = () => {
   return (
-    <div>PokeCard</div>
+    <>
+      <div className="pokemon pokemon-type">
+        <div className="img-container">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        </div>
+        <div className="info">
+          <span className="number">#001</span>
+          <h3 className="name">Bulbassaur</h3>
+          <h5 className="type">Grass</h5>
+        </div>
+      </div>
+    </>
   )
 }
 
