4dc6b076ad7d414fbf3a30a06ec946d50fd642f6
thiago <tmg3.mn@gmail.com>
2023-01-23 10:24:57 -0300

    Componente 'PokeCard' adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
+import PokeCard from './Components/pokeCard/PokeCard';
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
       <div className="poke-container" id="pokecontainer">
-        PokeCard
+        <PokeCard />
       </div>
     </div>
   );
