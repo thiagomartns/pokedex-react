bb3fe3a76d3b2d532efb2865189f3fe5732b008e
thiago <tmg3.mn@gmail.com>
2023-01-23 10:23:14 -0300

    HTML e estilos adicionados no componente raiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
+      <div className="poke-container" id="pokecontainer">
+        PokeCard
+      </div>
     </div>
   );
 }
